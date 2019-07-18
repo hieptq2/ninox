403ca5d222da561455456975e865e378a708fc01
hieptq2 <hieptq11@gmail.com>
2019-07-18 08:57:06 +0700

    File re-structure

--- html/assets/config.rb
+++ html/assets/config.rb
@@ -1,6 +1,10 @@
 http_path 			= "/"
 css_dir 				= "css"
 sass_dir 				= "scss"
-cache 					= false
-output_style 		= :expanded # :expanded :nested :compact :compressed
-# line_comments 	= false
+cache 					= true
+sourcemap 			= true
+line_comments 	= false
+output_style 		= :compressed 
+
+
+# :expanded :nested :compact :compressed
